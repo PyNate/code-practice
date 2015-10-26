3cbc578992e893ca29637cc117bbae03f35db799
Nathan Leonard <n.leonard668@gmail.com>
2015-10-26 18:01:28 -0500

    add handling of nested arrays and objects

--- challenges/objectSearch.js
+++ challenges/objectSearch.js
@@ -1,9 +1,22 @@
 function objectSearch(object, regex) {
   var result = [];
-  for ( key in object ) {
-    if ( object[key].match(regex) ) {
-      result.push(key);
+
+  
+  function recursiveSearch(value, currentLoc)
+    if ( typeof value !== 'object' ) {
+      if ( value.match(regex) ) {
+        result.push(currentLoc);
+      }
+    } else if ( Array.isArray(value) ) {
+      for ( var i = 0; i < value.length; i++ ) {
+        recursiveSearch(value[i], currentLoc + '[' + i + ']');
+      }
+    } else {
+      for ( key in value ) {
+        recursiveSearch(value[key], currentLoc + '.' + key);
+      }
     }
-  }
+
+  recursiveSearch(object, '');
   return result;
 }
