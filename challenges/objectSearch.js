64411baf8d3d3c9b3de5296948b9cc0fa83362c4
Nathan Leonard <n.leonard668@gmail.com>
2015-10-27 09:08:06 -0500

    remove console logs

--- challenges/objectSearch.js
+++ challenges/objectSearch.js
@@ -2,14 +2,13 @@ function objectSearch(object, regex) {
   var result = [];
 
   function recursiveSearch(value, currentLoc) {
-    console.log(value, value.visited);
     if ( typeof value !== 'object' ) {
       if ( value.match(regex) ) {
         result.push(currentLoc);
       }
     } else if ( Array.isArray(value) ) {
       for ( var i = 0; i < value.length; i++ ) {
-        if ( typeof value[i] === 'string' || typeof value[i] === 'object' ) {      
+        if ( typeof value[i] === 'string' || typeof value[i] === 'object' ) {
           try {
             recursiveSearch(value[i], currentLoc + '[' + i + ']');
           } catch (err) {
@@ -21,7 +20,7 @@ function objectSearch(object, regex) {
       if ( !value.visited ) {
         value.visited = true;
         for ( var key in value ) {
-          if ( typeof value[key] === 'string' || typeof value[key] === 'object' ) {        
+          if ( typeof value[key] === 'string' || typeof value[key] === 'object' ) {
             try {
               if ( currentLoc.length === 0 ) recursiveSearch(value[key], currentLoc + key);
               else recursiveSearch(value[key], currentLoc + '.' + key);
