317ebb50c92c1617454f71aad7e8709a79d904bb
Nathan Leonard <n.leonard668@gmail.com>
2015-10-26 21:30:52 -0500

    add error handling

--- challenges/objectSearch.js
+++ challenges/objectSearch.js
@@ -2,25 +2,34 @@ function objectSearch(object, regex) {
   var result = [];
 
   function recursiveSearch(value, currentLoc) {
+    console.log(value, value.visited);
     if ( typeof value !== 'object' ) {
       if ( value.match(regex) ) {
         result.push(currentLoc);
       }
     } else if ( Array.isArray(value) ) {
       for ( var i = 0; i < value.length; i++ ) {
-        if ( typeof value[i] === 'string' || typeof value[i] === 'object' ) {        
-          recursiveSearch(value[i], currentLoc + '[' + i + ']');
+        if ( typeof value[i] === 'string' || typeof value[i] === 'object' ) {      
+          try {
+            recursiveSearch(value[i], currentLoc + '[' + i + ']');
+          } catch (err) {
+            continue;
+          }
         }
       }
     } else {
       if ( !value.visited ) {
-        for ( key in value ) {
+        value.visited = true;
+        for ( var key in value ) {
           if ( typeof value[key] === 'string' || typeof value[key] === 'object' ) {        
-            if ( currentLoc.length === 0 ) recursiveSearch(value[key], currentLoc + key);
-            else recursiveSearch(value[key], currentLoc + '.' + key);
+            try {
+              if ( currentLoc.length === 0 ) recursiveSearch(value[key], currentLoc + key);
+              else recursiveSearch(value[key], currentLoc + '.' + key);
+            } catch (err) {
+              continue;
+            }
           }
         }
-        value.visited = true;
       }
     }
   }
