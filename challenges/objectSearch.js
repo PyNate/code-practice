e653e26ee564d1ba8ebc51f955221d413c3cd8b6
Nathan Leonard <n.leonard668@gmail.com>
2015-10-27 10:35:17 -0500

    add error handling

--- challenges/objectSearch.js
+++ challenges/objectSearch.js
@@ -2,17 +2,20 @@ function objectSearch(object, regex) {
   var result = [];
 
   function recursiveSearch(value, currentLoc) {
+    if ( typeof value === object && !Object.isExtensible(value) ) console.log("Frozen!");
     if ( typeof value !== 'object' ) {
       if ( value.match(regex) ) {
         result.push(currentLoc);
       }
     } else if ( Array.isArray(value) ) {
       for ( var i = 0; i < value.length; i++ ) {
-        if ( typeof value[i] === 'string' || typeof value[i] === 'object' ) {
+        if ( value [i] && (typeof value[i] === 'string' || typeof value[i] === 'object') ) {
           try {
             recursiveSearch(value[i], currentLoc + '[' + i + ']');
           } catch (err) {
-            continue;
+            console.log(err.name);
+            if ( err.name === "InvalidStateError" || err.name === "SecurityError" ) continue;
+            else throw err;
           }
         }
       }
@@ -20,12 +23,14 @@ function objectSearch(object, regex) {
       if ( !value.visited ) {
         value.visited = true;
         for ( var key in value ) {
-          if ( typeof value[key] === 'string' || typeof value[key] === 'object' ) {
+          if ( value[key] && (typeof value[key] === 'string' || typeof value[key] === 'object') ) {
             try {
               if ( currentLoc.length === 0 ) recursiveSearch(value[key], currentLoc + key);
               else recursiveSearch(value[key], currentLoc + '.' + key);
             } catch (err) {
-              continue;
+              console.log(err.name);
+              if ( err.name === "InvalidStateError" || err.name === "SecurityError" ) continue;
+              else throw err;
             }
           }
         }
